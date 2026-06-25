3eb7706b4325bc6874b639e33e86b1ed982ae9c2
Abdullah Amjad <shkabdullah268@gmail.com>
2026-06-25 03:04:15 -0700

    adding the deleteAtPoint Logic with Stream

--- LinkedLists/ll.py
+++ LinkedLists/ll.py
@@ -15,7 +15,7 @@ class LL:
     def traverse(self):
         pointer = self.head
         while pointer != None:
-            yield(pointer.data)
+            yield(pointer)
             pointer = pointer.next
 
     # can we do stream traverse means every iteration return something easy to reuse gonna took help from llm
@@ -24,16 +24,16 @@ class LL:
         stream = self.traverse()
         while True:
             try:
-                print(next(stream))
+                print(next(stream).data)
             except StopIteration:
                 break
     # now we are able to use the stream logic for search to delete or insert nice 
-    def deleteAtPoint(self, data, updatedData):
+    def ChangeAtPoint(self, data, updatedData):
         stream  = self.traverse() 
         value = next(stream)
         while True:
             try:
-                if data == value:
+                if data == value.data:
                     print("Found")
                     self.head.data = updatedData
                     print("Updated")
@@ -43,6 +43,27 @@ class LL:
             except Exception as e: 
                 print("NOT FOUND", e)
                 break
+    def DeleteAtPoint(self, data):
+        stream  = self.traverse() 
+        value = next(stream)
+        pointer = value
+        while True:
+            try: 
+                if pointer.data == data:
+                    self.head = value.next
+                    break
+                elif pointer.next.data == data: 
+                    value.next = value.next.next
+                else:
+                    pointer = next(stream)
+            except Exception as e:
+                print("Not Found")
+                break
+       
+
+
+       
+
 
     def search (self, data):
         stream = self.traverse() # -> generator 
@@ -52,7 +73,7 @@ class LL:
             try: 
                 i=i+1
 
-                if data == value:
+                if data == value.data:
                     print('Found')
                     break
                 else:
@@ -78,6 +99,13 @@ obj1.search(23)
 obj1.search(21)
 obj1.search(1)
 
+print("Updated")
+obj1.ChangeAtPoint(11,10)
+obj1.streamTraverse()
+
 print("DEL")
-obj1.deleteAtPoint(11,10)
+obj1.DeleteAtPoint(21)
+obj1.DeleteAtPoint(10)
+obj1.DeleteAtPoint(23)
+
 obj1.streamTraverse()
